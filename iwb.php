ea2aa5f372f4bce4dc05b9d3c8ce7c52f774feaf
Robby <robby.xp@gmail.com>
2017-07-07 12:03:36 +0800

    Fixed issues from Feed Validator

--- iwb.php
+++ iwb.php
@@ -2,10 +2,12 @@
 header('Content-Type: application/rss+xml;');
 echo '<?xml version="1.0" encoding="UTF-8" ?>';
 ?>
-<rss version="2.0">
+<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
 <channel>
 	<title>Iwanbanaran.com</title>
 	<link>http://iwanbanaran.com</link>
+	<description>Iwanbanaran.com RSS Feed</description>
+	<atom:link href="http://myxln.net/iwb.php" rel="self" type="application/rss+xml" />
 	<?php
 	$json = file_get_contents('http://iwanbanaran.com/wp-json/wp/v2/posts?context=embed');
 	$posts = json_decode($json);
@@ -13,7 +15,9 @@ echo '<?xml version="1.0" encoding="UTF-8" ?>';
 		<item>
 			<title><?php echo $post->title->rendered; ?></title>
 			<link><?php echo $post->link; ?></link>
-			<description><?php echo $post->excerpt->rendered; ?></description>
+			<description><![CDATA[<?php echo $post->excerpt->rendered; ?>]]></description>
+			<pubDate><?php echo date('r', strtotime($post->date.'+07:00')); ?></pubDate>
+			<guid><?php echo 'http://iwanbanaran.com/?p='.$post->id; ?></guid>
 		</item>
 	<?php } ?>
 </channel>
